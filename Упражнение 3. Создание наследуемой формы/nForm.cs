5770922aed1e564d17bb742996479ca0c6fdb4e0
Denis-Mosunov <dnsmosunov@gmail.com>
2024-07-25 18:39:54 +0300

    Add files via upload

---
+++ Упражнение 3. Создание наследуемой формы/nForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Упражнение_2
+{
+    public partial class nForm : Упражнение_2.TreeUgol
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
