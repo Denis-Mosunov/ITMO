16d80b6eabc3aeac2b1fdd044ccc4af3d8d7d7ce
Denis-Mosunov <dnsmosunov@gmail.com>
2024-07-31 16:02:07 +0300

    Лабораторная работа 1. Введение в разработку форм

---
+++ Упражнение 1. Настройка прямоугольной формы Win/WinFormsApp1/WinFormsApp1/nForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class nForm : Form
+    {
+        private bool nclose = false;
+        public new void Close()
+        {
+            nclose = true;
+            base.Close();
+        }
+
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBoxClose_CheckedChanged(object sender, EventArgs e)
+        {
+            if (nclose) return;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
